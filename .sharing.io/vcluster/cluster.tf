a56bb165b167dafa7bf3fc34f6dd042570584b2a
Hippie Hacker <hh@cncf.io>
2022-09-29 10:50:32 -0700

    vcluster is created

--- .sharing.io/vcluster/cluster.tf
+++ .sharing.io/vcluster/cluster.tf
@@ -11,3 +11,57 @@ resource "kubernetes_namespace" "work-namespace" {
     name = var.namespace
   }
 }
+resource "kubernetes_manifest" "cluster_vclusters_vcluster1" {
+  manifest = {
+    "apiVersion" = "cluster.x-k8s.io/v1beta1"
+    "kind"       = "Cluster"
+    "metadata" = {
+      "name"      = "vcluster1"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "controlPlaneRef" = {
+        "apiVersion" = "infrastructure.cluster.x-k8s.io/v1alpha1"
+        "kind"       = "VCluster"
+        "name"       = "vcluster1"
+      }
+      "infrastructureRef" = {
+        "apiVersion" = "infrastructure.cluster.x-k8s.io/v1alpha1"
+        "kind"       = "VCluster"
+        "name"       = "vcluster1"
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "vcluster_vclusters_vcluster1" {
+  manifest = {
+    "apiVersion" = "infrastructure.cluster.x-k8s.io/v1alpha1"
+    "kind"       = "VCluster"
+    "metadata" = {
+      "name"      = "vcluster1"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "controlPlaneEndpoint" = {
+        "host" = ""
+        "port" = 0
+      }
+      "helmRelease" = {
+        "chart" = {
+          "name"    = null
+          "repo"    = null
+          "version" = null
+        }
+        "values" = <<-EOT
+        service:
+          type: NodePort
+        syncer:
+          extraArgs:
+            - --tls-san=${var.tls-san}
+        EOT
+      }
+      "kubernetesVersion" = var.k8s-version
+    }
+  }
+}
