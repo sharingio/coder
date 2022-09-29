8850ed7e5eda8979030b3affd7e1cfebac7d632c
Hippie Hacker <hh@cncf.io>
2022-09-29 11:49:51 -0700

    Add kubectl wait local-exec until provisioned

--- .sharing.io/vcluster/cluster.tf
+++ .sharing.io/vcluster/cluster.tf
@@ -35,6 +35,12 @@ resource "kubernetes_manifest" "cluster_vclusters_vcluster1" {
 }
 
 resource "kubernetes_manifest" "vcluster_vclusters_vcluster1" {
+  provisioner "local-exec" {
+    command = "kubectl wait --for=condition=Ready --timeout=30s -n ${var.namespace} cluster vcluster1"
+  }
+  provisioner "local-exec" {
+    command = "kubectl get secrets -n ${var.namespace} vcluster1-kubeconfig -o jsonpath={.data.value} | base64 -d > kubeconfig"
+  }
   manifest = {
     "apiVersion" = "infrastructure.cluster.x-k8s.io/v1alpha1"
     "kind"       = "VCluster"
@@ -65,3 +71,19 @@ resource "kubernetes_manifest" "vcluster_vclusters_vcluster1" {
     }
   }
 }
+
+# This is generated from the vcluster...
+# Need to find a way for it to wait before running, so that the secret exists
+data "kubernetes_resource" "kubeconfig" {
+  api_version = "v1"
+  kind        = "Secret"
+  depends_on = [
+    kubernetes_manifest.vcluster_vclusters_vcluster1
+  ]
+  metadata {
+    name      = "vcluster-kubeconfig"
+    namespace = var.namespace
+  }
+}
+
+# We'll need to use the kubeconfig from above to provision the coder/pair environment
