53ffc64fc379ceee1ec73f866d48fee39b5eef78
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-10-03 16:15:10 +1300

    Update vcluster integration and code server

--- examples/templates/vcluster/cluster.tf
+++ examples/templates/vcluster/cluster.tf
@@ -11,6 +11,19 @@ terraform {
   }
 }
 
+# provider "kubernetes" {
+#   alias                  = "vcluster"
+#   host                   = yamldecode(data.kubernetes_resource.kubeconfig.data)["value"]["clusters"][0]["cluster"]["server"]
+#   client_certificate     = base64decode(yamldecode(data.kubernetes_resource.kubeconfig.data)["value"]["users"][0]["user"]["client-certificate-data"])
+#   client_key             = base64decode(yamldecode(data.kubernetes_resource.kubeconfig.data)["value"]["users"][0]["user"]["client-key-data"])
+#   cluster_ca_certificate = base64decode(yamldecode(data.kubernetes_resource.kubeconfig.data)["value"]["clusters"][0]["cluster"]["certificate-authority-data"])
+# }
+
+variable "base_domain" {
+  type    = string
+  default = "test.pair.sharing.io"
+}
+
 data "coder_workspace" "me" {}
 
 resource "coder_agent" "main" {
@@ -64,6 +77,9 @@ resource "kubernetes_manifest" "cluster" {
     "metadata" = {
       "name"      = data.coder_workspace.me.name
       "namespace" = data.coder_workspace.me.name
+      "labels" = {
+        "cluster-name" = data.coder_workspace.me.name
+      }
     }
     "spec" = {
       "controlPlaneRef" = {
@@ -80,13 +96,22 @@ resource "kubernetes_manifest" "cluster" {
   }
 }
 
+# data "kubernetes_resource" "cluster-kubeconfig" {
+#   api_version = "v1"
+#   kind        = "Secret"
+#   metadata {
+#     name      = "${data.coder_workspace.me.name}-kubeconfig"
+#     namespace = data.coder_workspace.me.name
+#   }
+
+#   depends_on = [
+#     kubernetes_namespace.workspace,
+#     kubernetes_manifest.cluster,
+#     kubernetes_manifest.vcluster
+#   ]
+# }
+
 resource "kubernetes_manifest" "vcluster" {
-  provisioner "local-exec" {
-    command = "kubectl wait --for=condition=Ready --timeout=999s -n ${data.coder_workspace.me.name} cluster ${data.coder_workspace.me.name}"
-  }
-  provisioner "local-exec" {
-    command = "kubectl get secrets -n ${data.coder_workspace.me.name} ${data.coder_workspace.me.name}-kubeconfig -o jsonpath={.data.value} | base64 -d > kubeconfig"
-  }
   manifest = {
     "apiVersion" = "infrastructure.cluster.x-k8s.io/v1alpha1"
     "kind"       = "VCluster"
@@ -111,70 +136,102 @@ resource "kubernetes_manifest" "vcluster" {
           type: NodePort
         syncer:
           extraArgs:
-            - --tls-san="${data.coder_workspace.me.name}.sanskar.pair.shairng.io"
+            - --tls-san="${data.coder_workspace.me.name}.${var.base_domain}"
+            - --tls-san="${data.coder_workspace.me.name}.${data.coder_workspace.me.name}.svc"
         EOT
       }
     }
   }
 }
 
-resource "kubernetes_manifest" "ingress_clusters" {
+resource "kubernetes_manifest" "configmap_vclusters_vcluster_init" {
   manifest = {
-    "apiVersion" = "networking.k8s.io/v1"
-    "kind"       = "Ingress"
+    "apiVersion" = "v1"
+    "data" = {
+      "cool.yaml" = <<-EOT
+      apiVersion: v1
+      kind: ServiceAccount
+      metadata:
+        name: coder
+        namespace: default
+      ---
+      apiVersion: rbac.authorization.k8s.io/v1
+      kind: ClusterRoleBinding
+      metadata:
+        name: coder
+      roleRef:
+        apiGroup: rbac.authorization.k8s.io
+        kind: ClusterRole
+        name: cluster-admin
+      subjects:
+        - kind: ServiceAccount
+          name: coder
+          namespace: default
+      ---
+      apiVersion: apps/v1
+      kind: StatefulSet
+      metadata:
+        name: code-server
+        namespace: default
+      spec:
+        selector:
+          matchLabels:
+            app: code-server
+        serviceName: code-server
+        template:
+          metadata:
+            labels:
+              app: code-server
+          spec:
+            serviceAccountName: coder
+            securityContext:
+              runAsUser: 1000
+              fsGroup: 1000
+            containers:
+              - name: code-server
+                image: codercom/enterprise-base:ubuntu
+                command: ${jsonencode(["sh", "-c", coder_agent.main.init_script])}
+                securityContext:
+                  runAsUser: 1000
+                env:
+                  - name: CODER_AGENT_TOKEN
+                    value: ${coder_agent.main.token}
+      EOT
+    }
+    "kind" = "ConfigMap"
     "metadata" = {
-      "annotations" = {
-        "nginx.ingress.kubernetes.io/backend-protocol" = "HTTPS"
-        "nginx.ingress.kubernetes.io/ssl-redirect"     = "true"
-      }
+      "name"      = "vcluster-instance-init"
+      "namespace" = data.coder_workspace.me.name
+    }
+  }
+}
+
+resource "kubernetes_manifest" "clusterresourceset_vclusters_vcluster_init" {
+  manifest = {
+    "apiVersion" = "addons.cluster.x-k8s.io/v1beta1"
+    "kind"       = "ClusterResourceSet"
+    "metadata" = {
       "name"      = data.coder_workspace.me.name
       "namespace" = data.coder_workspace.me.name
     }
     "spec" = {
-      "ingressClassName" = "contour-external"
-      "rules" = [
+      "clusterSelector" = {
+        "matchLabels" = {
+          "cluster-name" = data.coder_workspace.me.name
+        }
+      }
+      "resources" = [
         {
-          "host" = "${data.coder_workspace.me.name}.sanskar.pair.sharing.io"
-          "http" = {
-            "paths" = [
-              {
-                "backend" = {
-                  "service" = {
-                    "name" = data.coder_workspace.me.name
-                    "port" = {
-                      "number" = 443
-                    }
-                  }
-                }
-                "path"     = "/"
-                "pathType" = "ImplementationSpecific"
-              },
-            ]
-          }
+          "kind" = "ConfigMap"
+          "name" = "vcluster-instance-init"
         },
       ]
-      "tls" = [
-        {
-          "hosts" = [
-            "${data.coder_workspace.me.name}.sanskar.pair.sharing.io",
-          ]
-        },
-      ]
+      "strategy" = "ApplyOnce"
     }
   }
 }
+
 # This is generated from the vcluster...
 # Need to find a way for it to wait before running, so that the secret exists
-# data "kubernetes_resource" "kubeconfig" {
-#   api_version = "v1"
-#   kind        = "Secret"
-#   depends_on = [
-#     kubernetes_manifest.vcluster
-#   ]
-#   metadata {
-#     name      = "vcluster-kubeconfig"
-#     namespace = var.namespace
-#   }
-# }
 
 # We'll need to use the kubeconfig from above to provision the coder/pair environment
